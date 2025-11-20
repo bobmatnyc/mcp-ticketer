cbbd7026aaa3e2290676423243fe0248626f6ad9
Bob Matsuoka <bob@matsuoka.com>
2025-11-20 13:39:59 -0500

    chore: bump version to 1.0.1

--- src/mcp_ticketer/__version__.py
+++ src/mcp_ticketer/__version__.py
@@ -1,6 +1,6 @@
 """Version information for mcp-ticketer package."""
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __version_info__ = tuple(int(part) for part in __version__.split("."))
 
 # Package metadata
